577cf11545d5ca38d1c6ca68803cac5261b1a66b
hadeel999 <hadeelahmadsaleh@gmail.com>
2022-05-11 16:29:19 +0300

    Add first work for Task17

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Home from './components/home/Home';
 const travelData=require("./data/db.json");
+import { Routes, Route, Link } from "react-router-dom";
+import TourDetails from './components/TourDetails/TourDetails';
 
 function App() {
   return (
     <>
-    <Home data={travelData}/>
+    <Routes>
+      <Route path='/' element= {<Home data={travelData}/>}/>
+      <Route path='/city/:id' element= {<TourDetails data={travelData}/>}/>
+    </Routes>
+    
     </>
   );
 }
---
+++ demo/src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import {Link} from "react-router-dom";
+export default function Navbar(props){
+    return(
+        <nav>
+            <Link to= "/">Home</Link>
+        </nav>
+    )
+}
